565f418c5ae3f04f0ccad3c650e674e874717e29
Leonardo Mizoguti <leomizo2@gmail.com>
2015-10-08 13:45:32 -0300

    Corrected problem with birthdate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,6 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
-		if param_birthdate
-			@user.birthdate = param_birthdate
-		end
 		if @user.save
 	      render json: {success: true}
 	    else
